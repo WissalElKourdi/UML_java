e09b54226b89654241b888a0a0762cbbaae0cb8d
Chaouch Sirine <sirine.chaouch2001@gmail.com>
2022-11-16 09:29:02 +0100

    création premieres classes

---
+++ src/main/java/History.java
@@ -0,0 +1,28 @@
+import java.util.Date;
+
+public class History {
+    long HistoryId ;
+    list<String> UserList;
+    long SourceId;
+    long DestId;
+    date Date;
+    String Message;
+
+
+
+    public ReturnHistory(){
+        System.out.println("TO DO: ReturnHistoryWithDest()");
+    }
+
+    public CreateHistory(){
+        System.out.println("TO DO: CreateHistory()");
+    }
+
+    public InsertMessage(){
+        System.out.println("TO DO: ReturnHistoryWithDest()");
+    }
+
+    public DeleteHistory(){
+        System.out.println("TO DO: CreateHistory()");
+    }
+}
---
+++ src/main/java/Notify.java
@@ -0,0 +1,11 @@
+public class Notify {
+    long NotifyId ;
+
+    public NotifyChangePseudo(){
+        System.out.println("TO DO: Notify Change Pseudo");
+    }
+
+    public NotifyNewClient(){
+        System.out.println("TO DO: NotifyNewClient");
+    }
+}
