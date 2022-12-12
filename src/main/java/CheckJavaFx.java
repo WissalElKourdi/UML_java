d6454dbd33eba2d13fe06be9d0c97dc1106e5e02
leoniegtt <leonie.goutte@gmail.com>
2022-12-12 16:42:57 +0100

    java fx

--- src/main/java/CheckJavaFx.java
+++
@@ -1,8 +0,0 @@
-import javafx.scene.paint.Color;
-
-public final class CheckJavaFx {
-    public static void main (String[] args) {
-        Color c = Color.RED;
-        System.out.println(c.getRed());
-    }
-}
