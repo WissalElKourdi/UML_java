06224051d4db49667c96538f8e2cd012836a338c
WissalElKourdi <102813723+WissalElKourdi@users.noreply.github.com>
2023-01-03 15:01:22 +0100

    connexion serveur multi-clients marche

--- src/main/java/communication/TCP_Server.java
+++ src/main/java/communication/TCP_Server.java
@@ -10,44 +10,17 @@ import java.time.LocalTime;
 import java.util.Scanner;
 
 public class TCP_Server {
-    public static void main(String[] args) throws IOException{
-        int port = 50000;
-        final ServerSocket socketserver ; // final indique que l'élément ne peut pas être changé dans la suite
-        final Socket clientSocket ;
-        final BufferedReader in;//Lire le texte reçu à partir de l'émetteur
-        final PrintWriter out; // Envoyer le texte saisi
-        final Scanner sc=new Scanner(System.in); //lire les entrées du clavier
+
+    public static void launchReceiverThread(Socket socket) {
 
         try {
-            socketserver= new ServerSocket(port);
-            System.out.println("Serveur est à l'écoute du port "+socketserver.getLocalPort());
-            clientSocket= socketserver.accept();
-            System.out.println("Connecté");
-            out = new PrintWriter(clientSocket.getOutputStream()); // get the output flow
-            in = new BufferedReader (new InputStreamReader(clientSocket.getInputStream())); //get the input flow
-            Thread envoi= new Thread(new Runnable() {// la création des 2 threads a pour but de permettre l'envoi et la réception simultanément
-                String Message;
-                public void run() {
-                    while (true) { //teste la connexion
-                        /*if(in.read() == -1) {// true if client is disconnected
-                            // Notify via terminal, close connection
-                            System.out.println("client disconnected. Socket closing...");
-                            socketserver.close();
-                        }*/
-                        Message = sc.nextLine();//stocke le texte. Cette méthode au scanner créé
-                        LocalTime time = LocalTime.now();
-                        //new History().Add_Message_History(Message+time);
-                        out.println(Message + " " + time); // renvoyer le message ( à changer si on va créer une classe display)
-                        out.flush(); // flush les buffers pour ne pas envoyer un null au client à la fin
-                    }
-                }
-            });
-            envoi.start();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             Thread recevoir= new Thread(new Runnable() {
                 String Message ;
                 @Override
                 public void run() {
+
                     try {
                         Message = in.readLine();
                         //tant que le client est connecté
@@ -58,17 +31,54 @@ public class TCP_Server {
                         //sortir de la boucle si le client a déconecté
                         System.out.println("Client déconnecté");
                         //fermer le flux et la session socket
-                        out.close();
-                        clientSocket.close();
-                        socketserver.close();
+                        socket.close();
+                        //socketserver.close();
                     } catch (IOException e) {
-                        e.printStackTrace();
+                        System.err.println(e.getMessage());
                     }
                 }
-            });
+            } );
             recevoir.start();
-        }catch (IOException e) {
-            e.printStackTrace();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
+    }
+
+    public static void SenderThread(Socket socket){
+        Scanner sc=new Scanner(System.in);
+        try {
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+        Thread envoi= new Thread(new Runnable() {// la création des 2 threads a pour but de permettre l'envoi et la réception simultanément
+            String Message;
+            public void run() {
+                while (true) { //teste la connexion
+                    Message = sc.nextLine();//stocke le texte. Cette méthode au scanner créé
+                    LocalTime time = LocalTime.now();
+                    //new History().Add_Message_History(Message+time);
+                    out.println(Message + " " + time); // renvoyer le message ( à changer si on va créer une classe display)
+                    out.flush(); // flush les buffers pour ne pas envoyer un null au client à la fin
+                }
+            }
+        });
+        envoi.start();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        int port = 50000;
+        try {
+            ServerSocket socketserver = new ServerSocket(port);
+            while (true) {
+                System.out.println("Serveur est à l'écoute du port " + socketserver.getLocalPort());
+                Socket clientSocket = socketserver.accept();
+                System.out.println("Connecté");
+                //TCP_Server.SenderThread(clientSocket);
+                TCP_Server.launchReceiverThread(clientSocket);
+            }
+        }catch(IOException e){
+                e.printStackTrace();
+            }
+
     }
 }
