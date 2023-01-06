db64c2949f8d1d99d1be35d309ae08df27b96138
Léonie <leonie.goutte@gmail.com>
2023-01-06 12:05:08 +0100

    suite interface - corrections loader

--- src/main/java/Interface/Controller.java
+++ src/main/java/Interface/Controller.java
@@ -1,8 +1,16 @@
 package Interface;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
     //insert on click events and other functions for interface-user interaction
     public void CancelClicked() {
         System.out.println("Button clicked!");
+    }
+
+    private Label label;
+    public void setLabelText(String text) {
+        label.setText(text);
     }
 }
