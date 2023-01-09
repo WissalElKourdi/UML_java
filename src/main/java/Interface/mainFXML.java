0e7d9a64f41846cfc461d5d70d0ae7313584d2f1
Léonie <leonie.goutte@gmail.com>
2023-01-09 14:29:46 +0100

    setup ok, début controller loginpage

--- src/main/java/Interface/mainFXML.java
+++ src/main/java/Interface/mainFXML.java
@@ -4,28 +4,27 @@ import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.fxml.*;
-import java.net.URL;
-import java.util.List;
-import javafx.event.*;
 
 public class mainFXML extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/login_page.fxml"));    //Tell the FXMLLoader where the FXML file is
+        Parent parent = loader.load();                     //create the view and link it with the Controller
+        Controller controller = loader.getController();
+        controller.setLabelText("Test Label");
+
+        //Preparing the Scene
+        Scene scene = new Scene(parent, 600, 300);
+
+        //Preparing the stage
+        stage.setTitle("Test stage");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
         Application.launch(args);
     }
 
-    @Override
-    public void start(Stage stage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/welcome_page.fxml"));    //Tell the FXMLLoader where the FXML file is
-        Parent parent = loader.load();                     //create the view and link it with the Controller
-        Controller controller = loader.getController();
-        controller.setLabelText("Injected Text");
-
-        Group root = new Group();
-        ObservableList list = root.getChildren();
-        list.add(NodeObject);
-
-        stage.setTitle("Hello World");
-        stage.setScene(new Scene(parent, 300, 275));
-        stage.show();
-    }
 }
