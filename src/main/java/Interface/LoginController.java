b081a4f2cab941342fa49b0e48b4a45449e5335d
Léonie <leonie.goutte@gmail.com>
2023-01-13 13:58:58 +0100

    test entrée pseudo

--- src/main/java/Interface/LoginController.java
+++ src/main/java/Interface/LoginController.java
@@ -1,10 +1,12 @@
 package Interface;
 
+import UDP.UDP_Server;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
@@ -21,13 +23,13 @@ public class LoginController {
     private Button cancelButton;
 
     @FXML
-    private TextField loginUsernameTextField;
-
-    @FXML
     private Button LoginButton;
 
     @FXML
     private TextField choose_username;
+
+    @FXML
+    private TextFlow returnText;
 
     @FXML
     void CancelClicked(ActionEvent event) {
@@ -59,7 +61,29 @@ public class LoginController {
     }
 
     @FXML
-    void saveUsername(ActionEvent event) {
+    void saveUsername(ActionEvent event) throws IOException {
+        //get new username and check that it's not already used
+        String name = choose_username.getText();
+        //if it's not, change to menu scene
+        if (UDP_Server.broadcast_Pseudo(name)) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Menu.fxml"));    //Tell the FXMLLoader where the FXML file is
+                Parent parent = loader.load();                     //create the view and link it with the Controller
+                Scene scene = new Scene(parent, 600, 300);
+                //Stage stage = new Stage();
+
+                //Preparing the stage
+                mainFXML.mainStage.setTitle("Chat App");
+                mainFXML.mainStage.setScene(scene);
+                mainFXML.mainStage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Text text = new Text ("This username is already taken, choose another one");
+            returnText.getChildren().add(text);
+        }
 
     }
 }
