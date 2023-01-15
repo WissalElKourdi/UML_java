76b351697fb449b876fa233dfe40238b344d2aa4
chaouch15 <sirine.chaouch2001@gmail.com>
2023-01-15 16:40:57 +0100

    test tcp client /srv javafx

--- src/main/java/communication/TCP_Client.java
+++ src/main/java/communication/TCP_Client.java
@@ -11,27 +11,21 @@ import java.sql.SQLException;
 
 public class TCP_Client extends Thread {
     public static void goClient(String MSG, int port) throws SQLException {
-        final Socket[] clientSocket = new Socket[1];
+       
         String DB_NAME = "DB_MSG.db";
         createDB DB = new createDB(DB_NAME);
         System.out.println("MSG histpry" +DB.selectAllMsgHistory(DB_NAME));
-        port = 5000;
-        int finalPort = port;
-        Thread envoi= new Thread(new Runnable() {
-       public void run(){
-        try {
+
+        Thread envoi = new Thread(new Runnable() {
+            public void run() {
 
 
-            clientSocket[0] = new Socket("localhost", finalPort);
-            TCP_Server.SenderThread(clientSocket[0], MSG);
-            TCP_Server.launchReceiverThread(clientSocket[0]);
+                    Socket clientSocket;
+                    //  TCP_Server.SenderThread(clientSocket, MSG);
 
-            DB.insertHistory(MSG, "10/12/22","sissou",clientSocket[0].getLocalSocketAddress().toString(), clientSocket[0].getPort(), DB_NAME);
+                    //  DB.insertHistory(MSG, "10/12/22", "sissou", clientSocket.getLocalSocketAddress().toString(), clientSocket[0].getPort(), DB_NAME);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-       }}); envoi.start();
+
+            }
+        }); envoi.start();
     }}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,4 +11,5 @@ module UML_java {
     //exports Test;
     exports UDP;
 
+
 }
