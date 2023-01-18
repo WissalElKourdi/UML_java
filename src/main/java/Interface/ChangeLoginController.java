d0c3a9e768571380eb964e499c4dba7fb51204d9
Léonie <leonie.goutte@gmail.com>
2023-01-18 17:15:47 +0100

    rajout verif username

--- src/main/java/Interface/ChangeLoginController.java
+++ src/main/java/Interface/ChangeLoginController.java
@@ -63,29 +63,36 @@ public class ChangeLoginController {
                 String name = NewLoginArea.getText();
                 //if it's not already used, change to menu scene
                 // new UDP_Client(port).start();
-                if (serv_udp.broadcast_ChangePseudo(name, port)) {
-                        try {
-                                createDB DB = new createDB(Name_DB);
+                if (LoginController.isValid(name)) {
+                        if (serv_udp.broadcast_ChangePseudo(name, port)) {
+                                try {
+                                        createDB DB = new createDB(Name_DB);
 
-                                System.out.println("ICIII"+DB.getMonPseudo(Name_DB));
+                                        System.out.println("ICIII" + DB.getMonPseudo(Name_DB));
 
-                                //UDP_Server.broadcast_connection(name, port);
+                                        //UDP_Server.broadcast_connection(name, port);
+                                        serv_udp.broadcast_end(port);
+                                        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Menu.fxml"));
+                                        Parent parent = loader.load();
+                                        Scene scene = new Scene(parent, 1200, 800);
+                                        scene.getStylesheets().add("/styles.css");
+                                        mainFXML.mainStage.setTitle("Chat App");
+                                        mainFXML.mainStage.setScene(scene);
+                                        mainFXML.mainStage.show();
+                                } catch (IOException e) {
+                                        e.printStackTrace();
+                                }
+                        } else {
                                 serv_udp.broadcast_end(port);
-                                FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Menu.fxml"));
-                                Parent parent = loader.load();
-                                Scene scene = new Scene(parent, 1200,800);
-                                scene.getStylesheets().add("/styles.css");
-                                mainFXML.mainStage.setTitle("Chat App");
-                                mainFXML.mainStage.setScene(scene);
-                                mainFXML.mainStage.show();
-                        } catch (IOException e) {
-                                e.printStackTrace();
+                                Text text = new Text("This username is already taken, choose another one");
+                                result.getChildren().clear();
+                                result.getChildren().add(text);
                         }
-                }else{
-                        serv_udp.broadcast_end(port);
-                        Text text = new Text ("This username is already taken, choose another one");
+                } else{
+                        Text text = new Text("Username should be between 5 and 15 characters and only contain letters and digits");
                         result.getChildren().clear();
                         result.getChildren().add(text);
                 }
+
         }
 }
